5ea994b227dc341f36eb743afedbc9bd86cacc5c
GoulartBruno <brunogoulartdev@gmail.com>
2023-02-23 22:37:33 +0100

    ✅ FAQ Done

--- src/components/Faq/faq.jsx
+++ src/components/Faq/faq.jsx
@@ -10,15 +10,21 @@ export default function Faq() {
   }, []);
 
   return (
-    <div>
-      <div>
-        <img src={icon} alt="Image icon" />
+    <div className="flex flex-col bg-gray-100 py-10 px-4 md:px-10 lg:px-20 xl:px-32">
+      <div className="bg-white rounded-2xl h-60 flex items-center justify-center mb-10 md:mt-5">
+        <img src={icon} alt="Image icon" className="h-12.5  mx-auto" />
       </div>
 
       {faq.map((element, index) => (
-        <div key={index}>
-          <p>{element.question}</p>
-          <p>{element.answer}</p>
+        <div
+          key={index}
+          className=" bg-white rounded-2xl shadow-md mb-8 md:mb10 lg:mb-12">
+          <p className=" font-dela text-black text-xl	 pt-2 mx-10 mt-10">
+            {element.question}
+          </p>
+          <p className=" border-t-[3px] text-sm	 font-Montserrat text-gray-600 pb-8 pt-2 mx-10 mt-10">
+            {element.answer}
+          </p>
         </div>
       ))}
     </div>
