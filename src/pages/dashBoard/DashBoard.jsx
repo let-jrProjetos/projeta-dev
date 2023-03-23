f150112be4b001c8e07721df31d109356a271194
MichelleAntunes <miichelleantuness@gmail.com>
2023-03-23 18:14:37 +0100

    🗃️Add ProjectCard to dashBoard

--- src/pages/dashBoard/DashBoard.jsx
+++ src/pages/dashBoard/DashBoard.jsx
@@ -1,8 +1,12 @@
 import React from "react";
-
+import project from "../../../db";
+import ProjectCard from "../../components/dashBoard/ProjectCard";
 export default function DashBoard() {
   return (
     <>
+      {project.map((project) => {
+        <ProjectCard />;
+      })}
       <div>DashBoard</div>
     </>
   );
