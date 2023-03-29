38c52e15a75ece0ca578235cefca9556a832b7c9
MichelleAntunes <miichelleantuness@gmail.com>
2023-03-29 19:57:05 +0200

    🎨 format of the code ProjectCard

--- src/components/DashBoard/ProjectCard.jsx
+++ src/components/DashBoard/ProjectCard.jsx
@@ -1,26 +1,24 @@
 import React from "react";
 
-export function ProjectCard({ project }) {
-  console.log(project);
-  console.log("michele");
+export function ProjectCard({ elementos }) {
+  console.log(elementos);
   return (
     <>
       <div className="card w-96 bg-base-100 shadow-xl">
         <figure>
-          <img
-            src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-            alt="Shoes"
-          />
+          <img src={elementos.image} alt={elementos.tittle} />
         </figure>
         <div className="card-body">
           <h2 className="card-title">
-            {project.tittle}
-            <div className="badge badge-secondary"></div>
+            <div className="badge badge-secondary">??</div>
           </h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <p>{elementos.descriptionOne}</p>
+          <p>{elementos.languages}</p>
           <div className="card-actions justify-end">
-            <div className="badge badge-outline">michelle</div>
-            <div className="badge badge-outline">outros</div>
+            <div className="badge badge-outline">
+              Front-End{elementos.front}
+            </div>
+            <div className="badge badge-outline">Bakc-End{elementos.back}</div>
           </div>
         </div>
       </div>
--- src/pages/dashBoard/DashBoard.jsx
+++ src/pages/dashBoard/DashBoard.jsx
@@ -3,12 +3,11 @@ import project from "../../../db.json";
 import { ProjectCard } from "../../components/dashBoard/projectCard";
 
 export default function DashBoard() {
-  console.log(project);
+  console.log("michelle");
   return (
     <div>
       {project.project.map((elementos, index) => (
-        // <div>{elementos.image}</div>
-        <ProjectCard project={project} key={index} />
+        <ProjectCard elementos={elementos} key={index} />
       ))}
     </div>
   );
