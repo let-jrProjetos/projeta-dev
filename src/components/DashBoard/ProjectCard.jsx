05e5b708a78b7faa6115b2de89fe066642d67487
MichelleAntunes <miichelleantuness@gmail.com>
2023-03-30 14:14:27 +0200

    💄 DashBoard Style without font and color

--- src/components/DashBoard/ProjectCard.jsx
+++ src/components/DashBoard/ProjectCard.jsx
@@ -1,31 +1,34 @@
 import React from "react";
 
 export function ProjectCard({ elementos }) {
-  console.log(elementos);
   return (
     <>
-      <div className="card w-96 shadow-xl bg-neutral-50 p-2">
+      <div className="card w-96 shadow-xl  bg-white p-2">
         <figure>
           <img src={elementos.image} alt={elementos.tittle} />
         </figure>
         <div className="flex justify-center  ">
-          <p className="text-black">{elementos.descriptionOne}</p>
+          <p className="text-black font-bold text-3xl p-1">
+            {elementos.descriptionOne}
+          </p>
+        </div>
+        <div className="card-actions justify-center p-2">
+          <div className="badge badge-outline p-1">Front-End</div>
+          <div className="badge badge-outline p-1">Bakc-End</div>
+          <div className="badge badge-outline p-1">Banco de Dados</div>
         </div>
 
         <div className="card-body">
           <p className="text-black">{elementos.descriptionTwo}</p>
-          <p className="text-black">{elementos.languages}</p>
-          <div className="card-actions justify-end">
-            <div className="badge badge-outline p-1">Front-End</div>
-            <div className="badge badge-outline p-1">Bakc-End</div>
-          </div>
+          <p className="text-black">Linguages: {elementos.languages}</p>
+
           <h2 className="card-title">
-            <div className=" rounded-lg bg-green-300 text-white  p-2">
-              Add to Learned{" "}
+            <div className=" rounded-lg bg-green-300 text-white  p-1">
+              Add to Learned
             </div>
           </h2>
           <h2 className="card-title">
-            <div className="bg-red-200 rounded-lg text-white p-2">Remove </div>
+            <div className="bg-red-200 rounded-lg text-white p-1">Remove </div>
           </h2>
         </div>
       </div>
--- src/pages/dashBoard/DashBoard.jsx
+++ src/pages/dashBoard/DashBoard.jsx
@@ -3,9 +3,8 @@ import project from "../../../db.json";
 import { ProjectCard } from "../../components/dashBoard/projectCard";
 
 export default function DashBoard() {
-  console.log("michelle");
   return (
-    <div class="flex flex-wrap md:flex-wrap justify-center items-center gap-2 bg-stone-300	p-4">
+    <div class="flex flex-wrap md:flex-wrap justify-center items-center gap-2 bg-grey-123	p-4">
       {project.project.map((elementos, index) => (
         <ProjectCard elementos={elementos} key={index} />
       ))}
