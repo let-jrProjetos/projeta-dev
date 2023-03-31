a9c1f0d4f9047c18d63b88dcfc4c007daba2cb11
LATerada <larissaakiterada@gmail.com>
2023-03-31 13:10:31 -0300

    :construction: import all the icons to SideBarMobile.jsx

--- src/components/Header/sideBar/SideBarMobile.jsx
+++ src/components/Header/sideBar/SideBarMobile.jsx
@@ -1,3 +1,29 @@
-export const SideBarMobile = () => {
-  return <></>;
+import {
+  FaFolderOpen,
+  FaFolderPlus,
+  FaPager,
+  FaQuestionCircle,
+  FaRegEnvelope,
+  FaRegSun,
+  FaSignOutAlt,
+} from "react-icons/fa";
+import { ProfilePhoto } from "../profilePhoto/ProfilePhoto";
+
+export const SideBarMobile = ({ user }) => {
+  return (
+    <div className="absolute right-0 top-14 w-[4.5rem] h-3/4 bg-white rounded-2xl border-2 border-gray-700 ">
+      <div className="p-4 flex flex-col text-[#C4C4C4] justify-center items-center gap-y-6 ">
+        <div className="text-black">Logo</div>
+        <FaPager />
+        <FaFolderOpen />
+        <FaFolderPlus />
+        <FaRegEnvelope />
+        <div className="w-9 border-b-2 border-[#C4C4C4]"></div>
+        <FaQuestionCircle />
+        <FaRegSun />
+        <ProfilePhoto user={user} />
+        <FaSignOutAlt />
+      </div>
+    </div>
+  );
 };
