e0618fbd203a4d69b40ae3bf8c349a90a9cea0f7
LATerada <larissaakiterada@gmail.com>
2023-03-31 15:08:56 -0300

    :construction: SideBarDesktop.jsx finished

--- src/components/Header/sideBar/SideBarDesktop.jsx
+++ src/components/Header/sideBar/SideBarDesktop.jsx
@@ -21,17 +21,75 @@ export const SideBarDesktop = ({
       onClick={() => setIsSideBarDesktop(false)}
     >
       <div className="z-100 absolute right-36 top-14 w-48 h-4/6 bg-white rounded-2xl border-2 ">
-        <div className="h-full p-4 flex flex-col text-[#C4C4C4] text-2xl justify-arround items-center gap-y-7 ">
-          <div className="text-black text-sm py-4">Logo</div>
-          <FaPager onClick={() => goToDashboard(navigate)} />
-          <FaFolderOpen onClick={() => goToProjects(navigate)} />
-          <FaFolderPlus onClick={() => goToProjectForm(navigate)} />
-          <FaRegEnvelope onClick={() => goToMessages(navigate)} />
-          <div className="w-9 border-b-2 border-[#C4C4C4] py-1"></div>
-          <FaQuestionCircle onClick={() => goToHelp(navigate)} />
-          <FaRegSun onClick={() => goToSettings(navigate)} />
+        <div className="h-full p-4 flex flex-col text-[#C4C4C4] text-xl justify-arround items-end gap-y-5 ">
+          <div className="text-black text-sm py-1">Logo</div>
+          <button
+            className="flex items-center"
+            onClick={() => goToDashboard(navigate)}
+          >
+            <span className="font-Montserrat text-sm text-gray-header px-2">
+              Dashboard
+            </span>
+            <FaPager />
+          </button>
+          <button
+            className="flex items-center"
+            onClick={() => goToProjects(navigate)}
+          >
+            <span className="font-Montserrat text-sm text-gray-header px-2">
+              Projetos
+            </span>
+            <FaFolderOpen />
+          </button>
+          <button
+            className="flex items-center"
+            onClick={() => goToProjectForm(navigate)}
+          >
+            <span className="font-Montserrat text-sm text-gray-header px-2">
+              Novo Projeto
+            </span>
+            <FaFolderPlus />
+          </button>
+          <button
+            className="flex items-center"
+            onClick={() => goToMessages(navigate)}
+          >
+            <span className="font-Montserrat text-sm text-gray-header px-2">
+              Mensagens
+            </span>
+            <FaRegEnvelope />
+          </button>
+
+          <div className="w-36 border-b-2 border-[#C4C4C4] py-1"></div>
+          <button
+            className="flex items-center"
+            onClick={() => goToHelp(navigate)}
+          >
+            <span className="font-Montserrat text-sm text-gray-header px-2">
+              Ajuda
+            </span>
+            <FaQuestionCircle />
+          </button>
+          <button
+            className="flex items-center"
+            onClick={() => goToSettings(navigate)}
+          >
+            <span className="font-Montserrat text-sm text-gray-header px-2">
+              Configurações
+            </span>
+            <FaRegSun />
+          </button>
+
           <ProfilePhoto user={user} />
-          <FaSignOutAlt onClick={() => setIsLogedIn(false)} />
+          <button
+            className="flex items-center"
+            onClick={() => setIsLogedIn(false)}
+          >
+            <span className="font-Montserrat text-sm text-gray-header px-2">
+              Sair
+            </span>
+            <FaSignOutAlt />
+          </button>
         </div>
       </div>
     </div>
