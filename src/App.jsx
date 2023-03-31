22238b0dca4db21f72d7d888cd3ced93b9e1fa35
LATerada <larissaakiterada@gmail.com>
2023-03-31 12:04:43 -0300

    :construction: Header moved to Router.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { Footer } from "./components/FooterComponent/Footer";
 import "./App.css";
 import { Router } from "./routes/Router";
-import { Header } from "./components/Header/Header";
 
 function App() {
   return (
     <div>
-      <Header />
       <Router />
       <Footer />
     </div>
