b43472c21aacf546995bc4d259666e252075772e
MichelleAntunes <miichelleantuness@gmail.com>
2023-03-31 15:33:21 +0200

    🚧 pagination

--- src/pages/dashBoard/DashBoard.jsx
+++ src/pages/dashBoard/DashBoard.jsx
@@ -3,8 +3,25 @@ import project from "../../../db.json";
 import { ProjectCard } from "../../components/dashBoard/projectCard";
 
 export default function DashBoard() {
+  const [itens, setItens] = useState([]);
+  const [itensPerPage, setItensPerPage] = useState(10);
+  const [currentPage, setCurrentPage] = useState(0);
+  const pages = Math.ceil(itens.length / itensPerPage);
+
   return (
     <div class="  flex flex-wrap md:flex-wrap justify-center gap-4 bg-grey-123	p-4 items-center ">
+      <div>
+        {Array.from(Array(pages), (item, index) => {
+          return (
+            <button
+              value={index}
+              onClick={(e) => setCurrentPage(Number(e.target.value))}
+            >
+              {index}
+            </button>
+          );
+        })}
+      </div>
       {project.project.map((elementos, index) => (
         <ProjectCard elementos={elementos} key={index} />
       ))}
