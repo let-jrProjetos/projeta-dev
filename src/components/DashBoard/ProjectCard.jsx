b35fa7f7e03bd45e589768baefea056a19a42b08
MichelleAntunes <miichelleantuness@gmail.com>
2023-04-04 17:45:40 +0200

    🏗️ building dashBoard

--- src/components/DashBoard/ProjectCard.jsx
+++ src/components/DashBoard/ProjectCard.jsx
@@ -3,10 +3,10 @@ import React from "react";
 export function ProjectCard({ elementos }) {
   return (
     <>
-      <div className="card w-96 shadow-xl   bg-white p-2  ">
+      <div className="card w-96 shadow-xl   bg-white  ">
         <figure>
           <img
-            className="h-36 w-52"
+            className="h-56 w-96"
             src={elementos.image}
             alt={elementos.tittle}
           />
@@ -16,8 +16,8 @@ export function ProjectCard({ elementos }) {
             {elementos.descriptionOne}
           </p>
         </div>
-        <div className="card-actions justify-center p-2 font-Montserrat">
-          <div className="badge badge-outline p-1">Front-End</div>
+        <div className="card-actions justify-center p-2 font-Montserrat ">
+          <div className="badge badge-outline px-2">Front-End</div>
           <div className="badge badge-outline p-1">Bakc-End</div>
           <div className="badge badge-outline p-1">Banco de Dados</div>
         </div>
@@ -28,16 +28,18 @@ export function ProjectCard({ elementos }) {
           </p>
           <hr />
           <div className="text-black opacity-50">
-            <p> Linguages: {elementos.languages}</p>
+            <p> Linguagens: {elementos.languages}</p>
             <p>Detalhes: {elementos.details}</p>
             <p> Prazo Sugerido: {elementos.prazo}</p>
           </div>
 
           <div className="card-title flex justify-around pt-8">
-            <h2 className=" rounded-lg bg-grey-bgGreen text-white  p-1">
-              Add to Learned
+            <h2 className=" rounded-lg bg-grey-bgGreen text-white px-4">
+              Adicionar
             </h2>
-            <h2 className="bg-grey-bgRed rounded-lg text-white p-1">Remove </h2>
+            <h2 className="bg-grey-bgRed rounded-lg text-white px-4">
+              Remover{" "}
+            </h2>
           </div>
         </div>
       </div>
