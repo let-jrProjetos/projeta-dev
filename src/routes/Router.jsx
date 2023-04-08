e61e4c9212e72663608b9d532c02f315a2c04f0d
LATerada <larissaakiterada@gmail.com>
2023-04-08 20:23:18 -0300

    :construction: Add ProjectDetails to "pages"

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -4,6 +4,7 @@ import AboutUsPage from "../pages/AboutUs/AboutUs";
 import DashboardPage from "../pages/Dashboard/Dashboard";
 import FaqPage from "../pages/Faq/faq";
 import HomePage from "../pages/Home/Home";
+import ProjectDetailsPage from "../pages/ProjectDetails/ProjectDetails";
 import MyProjectsPage from "../pages/MyProjects/MyProjects";
 import ProjectFormPage from "../pages/ProjectForm/ProjectForm";
 import ProfilePage from "../pages/Profile/Profile";
@@ -18,10 +19,11 @@ export const Router = () => {
         <Route path="/sobreNos" element={<AboutUsPage />} />
         <Route path="/perguntasFrequentes" element={<FaqPage />} />
         <Route path="/projetos" element={<DashboardPage />} />
-        <Route path="/meusProjetos" element={<MyProjectsPage />} />
-        <Route path="/novoProjeto" element={<ProjectFormPage />} />
-        <Route path="/meuPerfil" element={<ProfilePage />} />
-        <Route path="/editarPerfil" element={<SettingsPage />} />
+        <Route path="/projetos/:name" element={<ProjectDetailsPage />} />
+        <Route path="/meusProjetos/:id" element={<MyProjectsPage />} />
+        <Route path="/novoProjeto/:id" element={<ProjectFormPage />} />
+        <Route path="/meuPerfil/:id" element={<ProfilePage />} />
+        <Route path="/editarPerfil/:id" element={<SettingsPage />} />
       </Routes>
     </BrowserRouter>
   );
