d584b492ea31d76be970142671bb92dfea987c90
LATerada <larissaakiterada@gmail.com>
2023-04-08 19:41:50 -0300

    :sparkles: Add routes

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import AboutUs from "../components/AboutUs";
 import { Header } from "../components/Header/Header";
+import AboutUs from "../components/AboutUs";
 import HomePage from "../pages/Home";
 
 export const Router = () => {
@@ -9,7 +9,13 @@ export const Router = () => {
       <Header />
       <Routes>
         <Route index element={<HomePage />} />
-        <Route path="sobreNos" element={<AboutUs />} />
+        <Route path="/sobreNos" element={<AboutUsPage />} />
+        <Route path="/perguntasFrequentes" element={<FaqPage />} />
+        <Route path="/projetos" element={<DashboardPage />} />
+        <Route path="/meusProjetos" element={<MyProjectsPage />} />
+        <Route path="/novoProjeto" element={<ProjectFormPage />} />
+        <Route path="/meuPerfil" element={<ProfilePage />} />
+        <Route path="/editarPerfil" element={<SettingsPage />} />
       </Routes>
     </BrowserRouter>
   );
