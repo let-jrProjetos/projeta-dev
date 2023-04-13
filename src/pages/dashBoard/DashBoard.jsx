df5f344de684f25361b2118abd7a26d9362dc569
MichelleAntunes <miichelleantuness@gmail.com>
2023-04-13 16:26:23 +0200

    :construction: building pagination

--- src/pages/dashBoard/DashBoard.jsx
+++ src/pages/dashBoard/DashBoard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import project from "../../../db.json";
 import { ProjectCard } from "../../components/dashBoard/projectCard";
 
@@ -6,19 +6,32 @@ export default function DashBoard() {
   const [itens, setItens] = useState([]);
   const [itensPerPage, setItensPerPage] = useState(10);
   const [currentPage, setCurrentPage] = useState(0);
+
   const pages = Math.ceil(itens.length / itensPerPage);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(project.project)
+        .then((response) => response.json())
+        .then((data) => data);
+
+      setItens(result);
+    };
+    fetchData();
+  }, []);
 
   return (
     <div class="  flex flex-wrap md:flex-wrap justify-center gap-4 bg-grey-123	p-4 items-center ">
       <div>
+        <div>{pages}</div>
+
         {Array.from(Array(pages), (item, index) => {
           return (
-            <button
-              value={index}
-              onClick={(e) => setCurrentPage(Number(e.target.value))}
-            >
-              {index}
-            </button>
+            <>
+              <div>{pages}</div>
+              <div>{index}</div>
+              <div>{item}</div>
+            </>
           );
         })}
       </div>
