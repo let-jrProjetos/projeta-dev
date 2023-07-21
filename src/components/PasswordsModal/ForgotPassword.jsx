4e4c63e783bf55eb1880b574de2cc362af245598
MichelleAntunes <miichelleantuness@gmail.com>
2023-07-21 13:33:29 +0200

    � add modal to ForgotPassword without correct style

--- src/components/PasswordsModal/ForgotPassword.jsx
+++ src/components/PasswordsModal/ForgotPassword.jsx
@@ -13,17 +13,29 @@ export const ForgotPassword = ({ onClose }) => {
   };
 
   return (
-    <div
-      className={`modal-center modal-middle forgot-password-modal ${
-        onClose ? "block" : "hidden"
-      }`}
-    >
-      <div className="modal-box flex flex-col justify-center items-center">
+    <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-50">
+      <div className="modal-box flex flex-col justify-center items-center bg-white rounded-lg p-6">
+        <label
+          htmlFor="modalLogin"
+          className="btn bg-transparent border-none hover:bg-transparent font-Montserrat font-semibold text-base text-gray-header absolute right-2 top-2"
+          onClick={onClose}
+        >
+          ✕
+        </label>
         <div className="font-bold font-Dela text-center my-6">
           RECUPERAR SENHA
         </div>
+        <div className="flex flex-col justify-center items-center">
+          <p>
+            Esqueceu sua senha? Digite seu e-mail cadastrado que enviaremos uma
+            o código de ativação para redefinir sua senha.
+          </p>
+        </div>
 
-        <div className="w-11/12 flex flex-col space-y-6">
+        <form
+          onSubmit={submitFormForgotPassword}
+          className="w-11/12 flex flex-col space-y-6"
+        >
           <div className="flex flex-row justify-start items-center gap-1 bg-[#C3DCE3] px-4 py-2 rounded-2xl focus-within:outline focus-within:outline-[#2B788B]/80">
             <FaEnvelope className="fill-[#757575]" />
             <label htmlFor="email"></label>
@@ -40,16 +52,14 @@ export const ForgotPassword = ({ onClose }) => {
           </div>
 
           <div className="modal-action grid grid-cols-4">
-            <label
-              htmlFor="modalLogin"
+            <button
               type="submit"
               className="btn col-start-2 col-span-2 bg-blue-green border-2 border-transparent mt-2 mb-2 rounded-2xl font-Dela font-thin text-xs capitalize tracking-[.15em] hover:bg-white hover:border-2 hover:border-[#2B788B] hover:text-[#2B788B]"
-              onClick={submitFormForgotPassword}
             >
               Recuperar
-            </label>
+            </button>
           </div>
-        </div>
+        </form>
 
         <div className="font-Dela text-xs mb-6">
           Já possui cadastro?{" "}
