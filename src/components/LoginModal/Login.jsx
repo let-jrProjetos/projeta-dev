2345d95d7630371979a50294b0e21a298586ae70
MichelleAntunes <miichelleantuness@gmail.com>
2023-10-26 20:39:30 +0200

    fixing modal in create new account

--- src/components/LoginModal/Login.jsx
+++ src/components/LoginModal/Login.jsx
@@ -1,14 +1,13 @@
 import { useState } from "react";
 import { useForm } from "../../hooks/useForm";
-import { FaArrowRight, FaEnvelope, FaLock } from "react-icons/fa";
+import { FaArrowRight, FaEnvelope, FaLock, FaUser } from "react-icons/fa";
 import { ForgotPassword } from "../PasswordsModal/ForgotPassword";
 import { SingIn } from "../SingInModal/SingIn";
 
 export const Login = () => {
   const [displayPassword, setDisplayPassword] = useState(false);
-  const [displaySignUp, setDisplaySignUp] = useState(false);
 
-  const { form, onChange } = useForm({
+  const { form, onChange, resetForm } = useForm({
     email: "",
     password: "",
   });
@@ -26,31 +25,32 @@ export const Login = () => {
     setDisplayPassword(false);
   };
 
-  const openSignUpModal = () => {
-    setDisplaySignUp(true);
-  };
+  // const openSignUpModal = () => {
+  //   setDisplaySignUp(true);
+  // };
 
-  const closeSignUpModal = () => {
-    setDisplaySignUp(false);
-  };
+  // const closeSignUpModal = () => {
+  //   setDisplaySignUp(false);
+  // };
+
   return (
-    <div className="flex items-center justify-center h-screen ">
-      <div className="flex flex-col items-center justify-center ">
+    <div className="flex items-center justify-center h-screen">
+      <div className="flex flex-col items-center justify-center">
         <label
           htmlFor="modalLogin"
-          className="btn-link flex justify-space-between items-center gap-1 text-sm text-black no-underline hover:cursor-pointer "
+          className="btn-link flex justify-space-between items-center gap-1 text-sm text-black no-underline hover:cursor-pointer"
         >
           <div className="font-Montserrat font-bold">Entrar</div>
           <FaArrowRight />
         </label>
 
-        <input type="checkbox" id="modalLogin" className="modal-toggle " />
+        <input type="checkbox" id="modalLogin" className="modal-toggle" />
         <div
           className={`modal modal-center modal-middle flex items-center justify-center h-screen  ${
             displayPassword ? "hidden" : "block"
           }`}
         >
-          <div className="modal-box flex flex-col justify-center items-center bg-white w-full ">
+          <div className="modal-box flex flex-col justify-center items-center bg-white w-full">
             <label
               htmlFor="modalLogin"
               className="btn bg-transparent border-none hover:bg-transparent font-Montserrat font-semibold text-base text-gray-header absolute right-2 top-2"
@@ -117,24 +117,24 @@ export const Login = () => {
               </div>
 
               <div className="modal-action grid grid-cols-4">
-                <label
-                  htmlFor="modalLogin"
+                <button
                   type="submit"
                   className="btn col-start-2 col-span-2 bg-blue-green border-2 border-transparent mt-5 mb-10 rounded-2xl font-Dela font-thin text-xs capitalize tracking-[.15em] hover:bg-white hover:border-2 hover:border-[#2B788B] hover:text-[#2B788B]"
                 >
                   Entrar
-                </label>
+                </button>
               </div>
             </form>
 
-            <div className="font-Dela text-xs mb-6">
+            <div className="font-Dela text-xs mb-6 flex justify-center items-center ">
               Não tem uma conta?{" "}
-              <span
+              {/* <span
                 className="btn-link font-Dela text-xs text-blue-green no-underline hover:cursor-pointer"
                 onClick={openSignUpModal}
               >
                 Crie agora!
-              </span>
+              </span> */}
+              <SingIn />
             </div>
           </div>
         </div>
